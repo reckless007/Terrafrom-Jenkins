3bcfcf407f5d431129ffcad59f3f9db70d3f7be0
Divyanshu Shekhar <sdivyanshu@pisoftek.local>
2021-12-20 10:12:24 +0530

    first commit

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIA2TY422QKQBI2ZD5Y"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-1"
+  }
